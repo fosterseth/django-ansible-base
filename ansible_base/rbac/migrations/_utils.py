df394d0aee1eaaf22f830e6857fc3a7459049277
Alan Rominger <arominge@redhat.com>
2024-02-14 17:06:03 -0500

    Contract changes to migration function needed for AWX

--- ansible_base/rbac/migrations/_utils.py
+++ ansible_base/rbac/migrations/_utils.py
@@ -1,17 +1,21 @@
-def give_permissions(apps, rd, users=(), teams=(), obj=None):
+from django.db import models
+
+
+def give_permissions(apps, rd, users=(), teams=(), object_id=None, content_type_id=None):
     """
     Give user permission to an object, but for use in migrations
     rd - role definition to grant the user
     users - list of users to give this permission to
-    teams - list of teams to give this permission to
-    obj - object that this permission applies to
+    teams - list of teams to give this permission to, can be objects or id list
+
+    target object is implicitly specified by
+    object_id - primary key of the object permission will apply to
+    content_type_id - primary key of the content type for the object
     """
     ObjectRole = apps.get_model('dab_rbac', 'ObjectRole')
-    ContentType = apps.get_model('contenttypes', 'ContentType')
-    ct = ContentType.objects.get_for_model(obj)
 
     # Create the object role and add users to it
-    object_role_fields = dict(role_definition=rd, object_id=obj.pk, content_type=ct)
+    object_role_fields = dict(role_definition=rd, object_id=object_id, content_type_id=content_type_id)
     object_role, _ = ObjectRole.objects.get_or_create(**object_role_fields)
 
     if users:
@@ -26,8 +30,15 @@ def give_permissions(apps, rd, users=(), teams=(), obj=None):
         RoleUserAssignment.objects.bulk_create(user_assignments)
     if teams:
         RoleTeamAssignment = apps.get_model('dab_rbac', 'RoleTeamAssignment')
-        team_assignments = [
-            RoleTeamAssignment(object_role=object_role, team=team, **object_role_fields)
-            for team in teams
-        ]
+        # AWX has trouble getting the team object, conditionally accept team id list
+        if isinstance(teams[0], models.Model):
+            team_assignments = [
+                RoleTeamAssignment(object_role=object_role, team=team, **object_role_fields)
+                for team in teams
+            ]
+        else:
+            team_assignments = [
+                RoleTeamAssignment(object_role=object_role, team_id=team_id, **object_role_fields)
+                for team_id in teams
+            ]
         RoleTeamAssignment.objects.bulk_create(team_assignments)
--- test_app/tests/rbac/test_migrations.py
+++ test_app/tests/rbac/test_migrations.py
@@ -1,6 +1,7 @@
 import pytest
 from django.apps import apps
 from django.test.utils import override_settings
+from django.contrib.contenttypes.models import ContentType
 
 from ansible_base.rbac.migrations._managed_definitions import setup_managed_role_definitions
 from ansible_base.rbac.migrations._utils import give_permissions
@@ -56,7 +57,21 @@ def test_give_permissions(organization, inventory, inv_rd):
         inv_rd,
         users=[user],
         teams=[team],
-        obj=inventory
+        object_id=inventory.id,
+        content_type_id=ContentType.objects.get_for_model(inventory).id
     )
     assert RoleUserAssignment.objects.filter(user=user).exists()
+    assert RoleTeamAssignment.objects.filter(team=team).exists()
+
+
+@pytest.mark.django_db
+def test_give_permissions_by_id(organization, inventory, inv_rd):
+    team = Team.objects.create(name='ateam', organization=organization)
+    give_permissions(
+        apps,
+        inv_rd,
+        teams=[team.id],
+        object_id=inventory.id,
+        content_type_id=ContentType.objects.get_for_model(inventory).id
+    )
     assert RoleTeamAssignment.objects.filter(team=team).exists()
